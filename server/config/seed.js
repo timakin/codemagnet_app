5785596cf9de7dbd3b489ee68ec25364a305f736
timakin <timaki.st@gmail.com>
2015-01-16 15:26:45 +0900

    line style none

--- server/config/seed.js
+++ server/config/seed.js
@@ -8,6 +8,47 @@
 var Thing = require('../api/thing/thing.model');
 var User = require('../api/user/user.model');
 
+Thing.find({}).remove(function() {
+  Thing.create({
+    name: 'google',
+    url: 'http://google.com',
+    info: 'test',
+    active: 1,
+    upvotes: 5,
+    created_at: Date.now()
+  }, {
+    name: 'google',
+    url: 'http://google.com',
+    info: 'test',
+    active: 1,
+    upvotes: 5,
+    created_at: Date.now()-1000*60*60*24*1
+  }, {
+    name: 'google',
+    url: 'http://google.com',
+    info: 'test',
+    active: 1,
+    upvotes: 5,
+    created_at: Date.now()-1000*60*60*24*1
+  }, {
+    name: 'google',
+    url: 'http://google.com',
+    info: 'test',
+    active: 1,
+    upvotes: 5,
+    created_at: Date.now()-1000*60*60*24*2
+  }, {
+    name: 'google',
+    url: 'http://google.com',
+    info: 'test',
+    active: 1,
+    upvotes: 5,
+    created_at: Date.now()-1000*60*60*24*2
+  }, function() {
+    console.log('finished populating things');
+  });
+});
+
 User.find({}).remove(function() {
   User.create({
     provider: 'local',
