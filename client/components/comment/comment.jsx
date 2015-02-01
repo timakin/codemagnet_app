97ee686f59e9ac825e258f6dae683a3fc5fb4ea7
snghrym <hirayamaru@gmail.com>
2015-02-01 15:22:34 +0900

    Add: sendable comment form

--- client/components/comment/comment.jsx
+++ client/components/comment/comment.jsx
@@ -1,8 +1,37 @@
 /** @jsx React.DOM */
-var Comment = React.createClass({
-  render: function() {
-    return <div>this is comment</div>;
-  }
-});
 
-// React.render(<Comment />, ReactComment);
+// var request = require('../bower_components/superagent/superagent.js');
+
+var Comment = React.createClass({
+
+    getInitialState: function() {
+      return {description: ''};
+    },
+
+    handleChange: function(event,data) {
+      this.setState({description: event.target.value});
+    },
+
+    componentDidMount: function(){
+        console.log($);
+    },
+
+    sending: function(){
+      $.post('/api/comments/',
+        {
+          thingId: this.props._id,
+          description: this.state.description
+        },function(err,res){
+          console.log(err,res);
+        });
+    },
+
+    render: function() {
+        return (
+            <div className="comment">
+              <textarea type="text"  value={this.state.description} onChange={this.handleChange}></textarea>
+              <div class="button" onClick={this.sending}>送信</div>
+            </div>
+        );
+    }
+});
---
+++ server/api/comment/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var express = require('express');
+var controller = require('./comment.controller');
+
+var router = express.Router();
+
+router.get('/', controller.index);
+router.get('/:id', controller.show);
+router.post('/', controller.create);
+router.put('/:id', controller.update);
+router.patch('/:id', controller.update);
+router.delete('/:id', controller.destroy);
+router.put('/:id/upvote', controller.upvote);
+
+module.exports = router;
