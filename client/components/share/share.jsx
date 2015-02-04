568e388c450ab7f9622a5155df48c4fd984089cc
snghrym <hirayamaru@gmail.com>
2015-02-04 10:40:39 +0900

    Fix: getInitialStateError by share

--- client/components/share/share.jsx
+++ client/components/share/share.jsx
@@ -3,6 +3,10 @@
 var Share = React.createClass({
 
     getInitialState: function() {
+      return{
+          title:'',
+          text:''
+        }
     },
     handleChange: function(event,data) {
     },
@@ -16,8 +20,10 @@ var Share = React.createClass({
         return (
           <div className="share">
             <div className="twitter">
-              <a href="https://twitter.com/share" className="twitter-share-button" data-url={this.props.link} data-text={this.props.text} data-via="codemagnet" data-hashtags="codemagnet">Tweet</a>
-              <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+              Twit
+            </div>
+            <div className="facebook">
+              FbShare
             </div>
           </div>
         );
