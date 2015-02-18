2fb9a51f05c9a9316b1991dbb7122aa92af187ce
snghrym <hirayamaru@gmail.com>
2015-02-18 10:31:31 +0900

    Fix: directive loading

--- client/app/codepost/new/codepostNew.controller.js
+++ client/app/codepost/new/codepostNew.controller.js
@@ -3,4 +3,13 @@
 angular.module('codemagnetAppApp')
   .controller('CodepostNewCtrl', function ($scope, User, Auth) {
     $scope.errors = {};
+  })
+  .directive('Comment', function(reactDirective) {
+      return reactDirective(Comment);
+  })
+  .directive('Share', function(reactDirective) {
+      return reactDirective(Share);
+  })
+  .directive('Codepost', function(reactDirective) {
+      return reactDirective(Codepost);
   });
